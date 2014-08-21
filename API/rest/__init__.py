6cbbf49d66d28c46cdabf6e99f4ce25419101f76
Sier Violencia <sierisimo@Development-tr3sco.local>
2014-08-21 15:08:06 -0500

    First commit at module_design
    - Added setup.py for letting 'python setup.py sdist' make our package
    
    * Added __init__ files

---
+++ API/rest/__init__.py
@@ -0,0 +1,16 @@
+#
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.1"
+
+""" api: Gives all the special things about a REST API in python """
+
+__author__ = "Monica Ileanne, Sier Valencia"
+__credits__ = ["Monica Ileanne","Sier Valencia"]
+
+__license__ = "GPL"
+
+__maintainer__ = "Sier Violencia"
+__email__ = "sierisimo@gmail.com"
+
+__status__ = "Prototype"
---
+++ API/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name="APIExample",
+  url="https://github.com/GULAcatlan/PythonAndREST",
+  author="Monica Ileanne",
+  author_email="ileanne2008@gmail.com",
+  maintainer="Sier Valencia",
+  maintainer_email="sierisimo@gmail.com",
+  version="0.0.1dev",
+  packages=["rest"],
+  license="General Public License version 3",
+  long_description=open("README").read()
+)
